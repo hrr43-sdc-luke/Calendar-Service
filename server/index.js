8bf778ef752bedc672d30638c5023a95a83b802b
azdoolittle <drewdoolittle@gmail.com>
2020-02-14 08:27:53 -0800

    server API routes, config axios GET route, renamed to calendar-bundle.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const bp = require('body-parser');
 const ejs = require('ejs');
 const db = require('../database');
@@ -7,27 +6,6 @@ const db = require('../database');
 const port = process.env.PORT || 3005;
 
 const app = express();
-
-//add same address:3005
-//add Dan:4000
-
-const whitelist = [
-  'http://18.222.165.232:4000', //Dan proxy
-  'http://18.223.132.12:4000', //my proxy
-  'http://18.217.113.225:4000', //Yer proxy
-  'http://18.223.132.12:3005',
-  'http://localhost:3005' //my service
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
-      callback(null, true);
-      console.log(origin);
-    } else {
-      callback(new Error('Not allowed by CORS' + origin));
-    }
-  },
-};
 
 app.use(express.static('public'));
 app.use(bp.json());
@@ -37,8 +15,26 @@ app.get('/:id', (req, res) => {
   res.render('../public/index.html');
 });
 
-app.get('/calendar/:id/', cors(corsOptions), (req, res) => {
+app.get('/calendar/:id/', (req, res) => {
   db.get(req.params.id,
+    () => { res.sendStatus(400); },
+    (data) => { res.send(data); });
+});
+
+app.post('calendar/:id/', (req, res) => {
+  db.insertOne(req.params.id, year, month, day, timeslot,
+    () => { res.sendStatus(400); },
+    (data) => { res.send(data); });
+});
+
+app.put('calendar/:id/', (req, res) => {
+  db.updateOne(req.params.id, year, month, day, timeslot,
+    () => { res.sendStatus(400); },
+    (data) => { res.send(data); });
+});
+
+app.delete('calendar/:id', (req, res) => {
+  db.deleteOne(req.params.id,
     () => { res.sendStatus(400); },
     (data) => { res.send(data); });
 });
