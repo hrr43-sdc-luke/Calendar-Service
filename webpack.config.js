964c7ce296b66e462ddcbb2389af9f2d2166e4ee
azdoolittle <drewdoolittle@gmail.com>
2020-03-08 15:35:23 -0700

    Finalized public, components and configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client');
-var DIST_DIR = path.join(__dirname, '/public');
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-gzip-webpack-plugin')
+
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'calendar-bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -20,13 +23,31 @@ module.exports = {
         test: /\.jsx?/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
+      },
+    ],
+  },
+  plugins: [
+    new BrotliPlugin({
+      asset: '[file].br[query]',
+      algorithm: 'brotli',
+      test: /\.(jsx|js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      quality: 11,
+    }),
+    new BrotliPlugin({
+      asset: '[file].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(jsx|js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      quality: 11,
+    }),
+  ],
 };
